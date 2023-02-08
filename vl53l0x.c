2d42458d05ea89b40812197aaf6fd3fd66e28efe
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2023-02-09 01:52:55 +0300

    Implement check args and check i2c communication funcrions

--- vl53l0x.c
+++ vl53l0x.c
@@ -34,11 +34,54 @@
 // returns 0 if OK
 static int check_args(vl53l0x_dev_t *dev)
 {
+	if (!dev || !dev->ll->delay_ms || \
+		!dev->ll->i2c_write_reg || !dev->ll->i2c_write_reg_16bit || \
+		!dev->ll->i2c_write_reg_32bit || !dev->ll->i2c_read_reg || \
+		!dev->ll->i2c_read_reg_16bit || !dev->ll->i2c_read_reg_32_bit) {
+		return 1;
+	}
+
 	return 0;
+}
+
+// returns 0 if OK
+static int check_i2c_comm(vl53l0x_dev_t *dev)
+{
+	int ret = 0;
+
+	if (dev->ll->i2c_read_reg(REF_REG_0) != REF_REG_0_VAL) {
+		++ret;
+	}
+
+	if (dev->ll->i2c_read_reg(REF_REG_1) != REF_REG_1_VAL) {
+		++ret;
+	}
+
+	if (dev->ll->i2c_read_reg(REF_REG_2) != REF_REG_2_VAL) {
+		++ret;
+	}
+
+	if (dev->ll->i2c_read_reg_16bit(REF_REG_3) != REF_REG_3_VAL) {
+		++ret;
+	}
+
+	if (dev->ll->i2c_read_reg_16bit(REF_REG_4) != REF_REG_4_VAL) {
+		++ret;
+	}
+
+	return ret;
 }
 
 vl53l0x_ret_t vl53l0x_init(vl53l0x_dev_t *dev)
 {
+	if (check_args(dev)) {
+		return VL53L0X_FAIL;
+	}
+
+	if (check_i2c_comm(dev)) {
+		return VL53L0X_FAIL;
+	}
+
 	return VL53L0X_OK;
 }
 
