220a443e45d67c2ac6aef8648daacfd887111d82
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2023-02-13 12:31:45 +0300

    viewer: read and display serial data

--- viewer.py
+++ viewer.py
@@ -3,13 +3,18 @@ import tkinter as tk
 import threading
 import random
 import time
+import serial
+
+SERIAL_DEV_NAME = "/dev/tty.usbserial-120"
 
 root = tk.Tk()
 root.geometry("500x200")
 root.title("Range sensor viewer")
 
 range_label = tk.Label(root, text=str(random.randint(0, 1000)), font=('Arial', 100))
-range_label.pack(padx=20, pady=50)
+range_label.pack(pady=50)
+
+sensor = serial.Serial(SERIAL_DEV_NAME, baudrate=115200, timeout = 1)
 
 def thread_run(func):
     threading.Thread(target=func).start()
@@ -20,7 +25,12 @@ def serial_thread():
     @tkthread.current(root)
     def serial_read_func():
         while 1:
-            time.sleep(0.1)
-            range_label.config(text=str(random.randint(0, 1000)))
+            text_int = int(sensor.readline())
+            if text_int >= 8189:
+                text = "Too far"
+            else:
+                text = str(text_int/10) + " cm"
+
+            range_label.config(text=text)
 
 root.mainloop()
